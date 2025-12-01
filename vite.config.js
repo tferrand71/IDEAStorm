6c28034d5a80d076e8a99fef9fd2905354f42e18
tferrand71 <tobiasferrand6@gmail.com>
2025-12-01 16:16:14 +0100

    upgrade et deploy pour jeux en ligne

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/IDEAStorm/', // <--- AJOUTE CETTE LIGNE (Attention aux majuscules exactes de ton repo)
+    base: '/IDEAStorm/', // Important pour GitHub Pages
 })
