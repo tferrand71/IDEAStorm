898d4f1ad275d6e085e36118640f7b289db4dd19
tferrand71 <tobiasferrand6@gmail.com>
2025-12-03 08:27:33 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/IDEAStorm/', // <--- INDISPENSABLE POUR GITHUB PAGES
+    base: '/IDEAStorm/', // <--- INDISPENSABLE POUR QUE ÇA MARCHE EN LIGNE
 })
